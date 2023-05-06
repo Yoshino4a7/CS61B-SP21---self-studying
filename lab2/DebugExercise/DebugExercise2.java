9df766c41f44cd8357c73856ff080369782a44a6
natsumi <402561486@qq.com>
2023-05-06 16:07:08 +0800

    修改了lab2相关内容

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -6,34 +6,48 @@ package DebugExercise;
  */
 public class DebugExercise2 {
     /** Returns the max of a and b. Do not step into this function. */
-    public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
+//    public static int max(int a, int b) {
+//        int w = (b - a) >> 31;
+//        /* If you're stepping into this function, click the
+//           step out button because you're not going to learn anything. */
+//        int z = ~(b - a) >> 31;
+//
+//        int max = b & w | a & z;
+//        return max;
+//    }
 
-        int max = b & w | a & z;
-        return max;
+    public static int max(int a, int b) {
+        if(a>=b)
+        return a;
+        else
+            return b;
     }
+
 
 
     /** Returns the sum of a and b. Do not step into this function. */
-    public static int add(int a, int b) {
-        int x = a, y = b;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int xor, and, temp;
-        and = x & y;
-        xor = x ^ y;
+//    public static int add(int a, int b) {
+//        int x = a, y = b;
+//        /* If you're stepping into this function, click the
+//           step out button because you're not going to learn anything. */
+//        int xor, and, temp;
+//        and = x & y;
+//        xor = x ^ y;
+//
+//        while (and != 0) {
+//            and <<= 1;
+//            temp = xor ^ and;
+//            and &= xor;
+//            xor = temp;
+//        }
+//        return xor;
+//    }
 
-        while (and != 0) {
-            and <<= 1;
-            temp = xor ^ and;
-            and &= xor;
-            xor = temp;
-        }
-        return xor;
+    public static int add(int a, int b) {
+
+        return a+b;
     }
+
 
     /** Returns a new array where entry i is the max of
      * a[i] and b[i]. For example, if a = {1, -10, 3}
@@ -46,7 +60,7 @@ public class DebugExercise2 {
         }
         int[] returnArray = new int[a.length];
         for (int i = 0; i < a.length; i += 1) {
-            int biggerValue = max(a[i], b[i]);
+            int biggerValue = max(b[i],a[i]);
             returnArray[i] = biggerValue;
         }
 
@@ -58,7 +72,7 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            sum =  add(sum, x[i]);
             i = i + 1;
         }
         return sum;

147109d6b281455e37ba4e6f94b5a63bdf1daf09
natsumi <402561486@qq.com>
2023-05-30 14:57:47 +0800

    利用随机测试测试BuggyAList，BuggyAList的bug出现在删除数组最后一个元素时， 错误地将数组大小缩小1/4，只需每次缩小一个元素即可

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -1,6 +1,7 @@
 package randomizedtest;
 
 import edu.princeton.cs.algs4.StdRandom;
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -9,4 +10,74 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public  void testThreeAddThreeRemove(){
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+
+        correct.addLast(5);
+        correct.addLast(10);
+        correct.addLast(15);
+
+        broken.addLast(5);
+        broken.addLast(10);
+        broken.addLast(15);
+
+        assertEquals(correct.size(), broken.size());
+
+        assertEquals(correct.removeLast(), broken.removeLast());
+        assertEquals(correct.removeLast(), broken.removeLast());
+        assertEquals(correct.removeLast(), broken.removeLast());
+    }
+
+    @Test
+    public void testRandomCall(){
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+
+        int N = 500;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 2);
+            if (operationNumber == 0) {
+                // addLast
+
+                for (int j = 0; j < N; j += 1) {
+                    int randVal = StdRandom.uniform(0, 100);
+                    correct.addLast(randVal);
+                    broken.addLast(randVal);
+                }
+                for (int c = 0; c < N; c += 1) {
+                    assertEquals(correct.removeLast(), broken.removeLast());
+                }
+
+            } else if (operationNumber == 1) {
+                // size
+
+                assertEquals(correct.size(), broken.size());
+            }
+        }
+
+    }
+
+    @Test
+    public void testBuggyRandomcall(){
+
+        BuggyAList<Integer> broken = new BuggyAList<>();
+
+        int N = 3;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 2);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                broken.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = broken.size();
+                System.out.println("size: " + size);
+            }
+        }
+    }
+
 }
